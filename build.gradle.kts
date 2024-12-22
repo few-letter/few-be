3abdad22c59222bd5422e5019fd2a93b175c9f15
belljun3395 <195850@jnu.ac.kr>
2024-12-22 16:42:56 +0900

    refactor: allure 결과 서브 모듈에 미반영 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,6 @@ allprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
-        systemProperty("allure.results.directory", "$projectDir/build/allure-results")
     }
 
     sourceSets {
@@ -188,6 +187,7 @@ subprojects {
     tasks {
         test {
             useJUnitPlatform()
+            systemProperty("allure.results.directory", "$projectDir/build/allure-results")
         }
 
         register<Test>("architectureSpecTest") {
