a0bbe3107bb3cfaf3bbde00c4704a0ea1dbe70f5
belljun3395 <195850@jnu.ac.kr>
2024-12-30 23:31:27 +0900

    refactor: event, web 모듈에 testFixtures 플러그인 적용

--- library/event/build.gradle.kts
+++ library/event/build.gradle.kts
@@ -6,6 +6,11 @@ tasks.getByName("jar") {
     enabled = true
 }
 
+plugins {
+    /** test  fixtures */
+    id("java-test-fixtures")
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-json")
 }
