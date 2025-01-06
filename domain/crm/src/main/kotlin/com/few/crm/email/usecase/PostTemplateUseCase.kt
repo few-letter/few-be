d4e28bc5a8a4cbaa5ae887a00f849ee4a06e2203
belljun3395 <195850@jnu.ac.kr>
2025-01-06 21:23:41 +0900

    refactor: PostTemplateUseCase 리펙토링

--- domain/crm/src/main/kotlin/com/few/crm/email/usecase/PostTemplateUseCase.kt
+++ domain/crm/src/main/kotlin/com/few/crm/email/usecase/PostTemplateUseCase.kt
@@ -1,39 +1,49 @@
 package com.few.crm.email.usecase
 
 import com.few.crm.email.domain.EmailTemplate
-import com.few.crm.email.domain.EmailTemplateHistory
-import com.few.crm.email.repository.EmailTemplateHistoryRepository
+import com.few.crm.email.event.template.PostEmailTemplateEvent
 import com.few.crm.email.repository.EmailTemplateRepository
+import com.few.crm.email.service.HtmlValidator
 import com.few.crm.email.usecase.dto.PostTemplateUseCaseIn
 import com.few.crm.email.usecase.dto.PostTemplateUseCaseOut
+import com.few.crm.support.jpa.CrmTransactional
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
 @Service
 class PostTemplateUseCase(
     private val emailTemplateRepository: EmailTemplateRepository,
-    private val emailTemplateHistoryRepository: EmailTemplateHistoryRepository,
+    private val applicationEventPublisher: ApplicationEventPublisher,
+    private val htmlValidator: HtmlValidator,
 ) {
+    @CrmTransactional
     fun execute(useCaseIn: PostTemplateUseCaseIn): PostTemplateUseCaseOut {
         val id: Long? = useCaseIn.id
         val templateName = useCaseIn.templateName
         val subject: String? = useCaseIn.subject
         val version: Float? = useCaseIn.version
-        // TODO: body & variables 유효성 검사
-        val body = useCaseIn.body
-        val variables = useCaseIn.variables
+        var body = useCaseIn.body
+        if (!htmlValidator.isValidHtml(body)) {
+            throw IllegalArgumentException("Invalid HTML")
+        }
+        body = htmlValidator.prettyPrintHtml(body)!!
+        val bodyVariables: List<String> = htmlValidator.extractVariables(body).sorted()
 
-        val persistedTemplate: EmailTemplate? =
-            id?.let {
-                emailTemplateRepository
-                    .findById(it)
-                    .orElseThrow { IllegalArgumentException("Template not found") }
-            } ?: emailTemplateRepository
-                .findByTemplateName(templateName)
-                ?.let {
-                    throw IllegalArgumentException("Duplicate template name: $templateName")
-                }
+        val variables =
+            useCaseIn.variables
+                .filterNot {
+                    it.isBlank()
+                }.filterNot {
+                    it.isEmpty()
+                }.sorted()
 
-        return (
+        if (bodyVariables != variables) {
+            throw IllegalArgumentException("Variables do not match")
+        }
+
+        val persistedTemplate: EmailTemplate? = getEmailTemplate(id, templateName)
+
+        val modifiedOrNewTemplate =
             persistedTemplate
                 ?.modifySubject(subject)
                 ?.modifyBody(body, variables)
@@ -45,31 +55,45 @@ class PostTemplateUseCase(
                         variables = variables,
                     )
                 }
-        ).let { template ->
+
+        modifiedOrNewTemplate.let { template ->
             if (template.isNewTemplate()) {
                 emailTemplateRepository.save(template)
             } else {
-                template.updateVersion(version).let {
-                    emailTemplateRepository.save(it)
-                }
+                template.updateVersion(version)
             }
-            // TODO: Refactor to event
-        }.let { savedTemplate ->
-            emailTemplateHistoryRepository.save(
-                EmailTemplateHistory(
-                    templateId = savedTemplate.id!!,
-                    subject = savedTemplate.subject,
-                    body = savedTemplate.body,
-                    variables = savedTemplate.variables,
-                    version = savedTemplate.version,
-                ),
-            )
-        }.let { template ->
+        }
+
+        applicationEventPublisher.publishEvent(
+            PostEmailTemplateEvent(
+                templateId = modifiedOrNewTemplate.id!!,
+                eventType = "POST",
+            ),
+        )
+
+        return run {
             PostTemplateUseCaseOut(
-                id = template.templateId, // TODO Fix to template.id
-                templateName = templateName, // TODO Fix to template.templateName
-                version = template.version,
+                id = modifiedOrNewTemplate.id!!,
+                templateName = modifiedOrNewTemplate.templateName,
+                version = modifiedOrNewTemplate.version,
             )
         }
+    }
+
+    private fun getEmailTemplate(
+        id: Long?,
+        templateName: String,
+    ): EmailTemplate? {
+        if (id != null) {
+            return emailTemplateRepository
+                .findById(id)
+                .orElseThrow { IllegalArgumentException("Template not found") }
+        }
+
+        return emailTemplateRepository
+            .findByTemplateName(templateName)
+            ?.let {
+                throw IllegalArgumentException("Duplicate template name: $templateName")
+            }
     }
 }
