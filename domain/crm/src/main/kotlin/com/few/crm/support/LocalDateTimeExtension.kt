5c6ad2aaa7381ed83afa9abda4b5b5b7fccda507
belljun3395 <195850@jnu.ac.kr>
2025-01-07 13:44:35 +0900

    fix: 나노 세컨드 자릿수 문제 해결

--- domain/crm/src/main/kotlin/com/few/crm/support/LocalDateTimeExtension.kt
+++ domain/crm/src/main/kotlin/com/few/crm/support/LocalDateTimeExtension.kt
@@ -4,12 +4,12 @@ import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
 
-fun LocalDateTimeExtension.parseEventTime(date: String): LocalDateTime =
-    LocalDateTime.parse(date, LocalDateTimeExtension.eventTimeFormatter)
+fun LocalDateTimeExtension.parseEventTime(date: String): LocalDateTime = LocalDateTime.parse(date, LocalDateTimeExtension.timeFormatter)
 
-fun LocalDateTimeExtension.parseExpiredTime(date: String): LocalDateTime =
-    LocalDateTime.parse(date, LocalDateTimeExtension.expiredTimeFormatter)
+fun LocalDateTimeExtension.parseExpiredTime(date: String): LocalDateTime = LocalDateTime.parse(date, LocalDateTimeExtension.timeFormatter)
 
 fun LocalDateTime.toScheduleTime(): Instant =
     Instant.ofEpochSecond(
@@ -23,7 +23,12 @@ fun LocalDateTime.toScheduleTime(): Instant =
 
 class LocalDateTimeExtension {
     companion object {
-        val eventTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS")
-        val expiredTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+        val timeFormatter: DateTimeFormatter =
+            DateTimeFormatterBuilder()
+                .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
+                .optionalStart()
+                .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
+                .optionalEnd()
+                .toFormatter()
     }
 }
