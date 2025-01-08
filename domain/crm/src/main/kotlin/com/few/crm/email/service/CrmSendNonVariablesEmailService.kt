e672a15f7fff79de9f1271c24d530fc66eadb7be
belljun3395 <195850@jnu.ac.kr>
2025-01-08 21:23:51 +0900

    refactor: 도메인 이벤트 구현

--- domain/crm/src/main/kotlin/com/few/crm/email/service/CrmSendNonVariablesEmailService.kt
+++ domain/crm/src/main/kotlin/com/few/crm/email/service/CrmSendNonVariablesEmailService.kt
@@ -1,7 +1,10 @@
 package com.few.crm.email.service
 
+import com.few.crm.email.domain.EmailSendEventType
+import com.few.crm.email.domain.SentEmail
 import email.*
 import email.provider.CrmAwsSESEmailSendProvider
+import event.domain.PublishEvents
 import org.springframework.boot.autoconfigure.mail.MailProperties
 import org.springframework.stereotype.Component
 
@@ -13,6 +16,20 @@ data class SendEmailArgs(
     override val properties: String = "",
 ) : SendMailArgs<NonContent, String>
 
+data class SendEmailDto(
+    val to: String,
+    val subject: String,
+    val template: String,
+    val content: NonContent,
+    val properties: String = "",
+    val userExternalId: String,
+    val emailBody: String,
+    val destination: String,
+    val eventType: EmailSendEventType,
+) {
+    val emailArgs = SendEmailArgs(to, subject, template, content, properties)
+}
+
 @Component
 class CrmSendNonVariablesEmailService(
     mailProperties: MailProperties,
@@ -22,6 +39,18 @@ class CrmSendNonVariablesEmailService(
     override fun getHtml(args: SendEmailArgs): String {
         val context = EmailContext()
         return emailTemplateProcessor.process(args.template, context, EmailTemplateType.STRING)
+    }
+
+    @PublishEvents
+    fun send(args: SendEmailDto): SentEmail {
+        val emailArgs = args.emailArgs
+        return SentEmail(
+            userExternalId = args.userExternalId,
+            emailBody = args.emailBody,
+            destination = args.destination,
+            emailMessageId = send(emailArgs),
+            eventType = args.eventType,
+        )
     }
 }
 
