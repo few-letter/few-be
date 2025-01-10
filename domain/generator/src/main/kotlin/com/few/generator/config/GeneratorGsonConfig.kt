1000576147cd3c1d9438e2d4b0a88b58e7bb6a76
belljun3395 <195850@jnu.ac.kr>
2025-01-10 15:22:24 +0900

    feat: Gson LocalDateTimeAdapter 추가

--- domain/generator/src/main/kotlin/com/few/generator/config/GeneratorGsonConfig.kt
+++ domain/generator/src/main/kotlin/com/few/generator/config/GeneratorGsonConfig.kt
@@ -1,9 +1,13 @@
 package com.few.generator.config
 
+import com.google.gson.*
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.lang.reflect.Type
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Configuration
 class GeneratorGsonConfig {
@@ -12,10 +16,29 @@ class GeneratorGsonConfig {
     }
 
     @Bean(GSON_BEAN_NAME)
-    fun fewGson(): Gson =
+    fun gson(): Gson =
         GsonBuilder()
             .setLenient()
             .disableHtmlEscaping()
             .setPrettyPrinting()
+            .registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeAdapter())
             .create()
+}
+
+class LocalDateTimeAdapter :
+    JsonSerializer<LocalDateTime>,
+    JsonDeserializer<LocalDateTime> {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    override fun serialize(
+        src: LocalDateTime,
+        typeOfSrc: Type,
+        context: JsonSerializationContext,
+    ): JsonElement = JsonPrimitive(src.format(formatter))
+
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext,
+    ): LocalDateTime = LocalDateTime.parse(json.asString, formatter)
 }
