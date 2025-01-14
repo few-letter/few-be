7394063250f2771e45dec62c51190c4d871b32df
belljun3395 <195850@jnu.ac.kr>
2025-01-14 17:17:44 +0900

    refactor: 상수 enum으로 변경

--- domain/crm/src/main/kotlin/com/few/crm/email/event/send/handler/EmailDeliveryEventHandler.kt
+++ domain/crm/src/main/kotlin/com/few/crm/email/event/send/handler/EmailDeliveryEventHandler.kt
@@ -1,12 +1,12 @@
 package com.few.crm.email.event.send.handler
 
 import com.few.crm.email.event.send.EmailDeliveryEvent
+import com.few.crm.email.event.send.EmailSendStatus
 import com.few.crm.email.repository.EmailSendHistoryRepository
+import com.few.crm.support.jpa.CrmTransactional
 import event.EventHandler
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Propagation
-import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Component
@@ -15,14 +15,14 @@ class EmailDeliveryEventHandler(
 ) : EventHandler<EmailDeliveryEvent> {
     val log = KotlinLogging.logger {}
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @CrmTransactional
     override fun handle(event: EmailDeliveryEvent) {
         log.info { "Handling EmailDeliveryEvent: $event" }
         // TODO check emailSendHistory and update status if history is not found retry 3 times
         emailSendHistoryRepository
             .findByEmailMessageId(event.messageId)
             ?.let {
-                it.sendStatus = event.eventType.uppercase(Locale.getDefault())
+                it.sendStatus = EmailSendStatus.DELIVERY.name
                 emailSendHistoryRepository.save(it)
             }
             ?: log.error { "EmailSendHistory not found for messageId: ${event.messageId}" }
