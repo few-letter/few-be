4f9dde225ac9a999418f1b065a90d83e344d25ed
Jihun-Hwang <hun.ca.dev@gmail.com>
2025-06-01 01:02:52 +0900

    refactor: 각 단계별 수행시간 로깅 추가

--- domain/generator/src/main/kotlin/com/few/generator/usecase/SchedulingUseCase.kt
+++ domain/generator/src/main/kotlin/com/few/generator/usecase/SchedulingUseCase.kt
@@ -3,6 +3,7 @@ package com.few.generator.usecase
 import com.few.generator.domain.Category
 import com.few.generator.domain.GenType
 import com.few.generator.domain.ProvisioningContents
+import com.few.generator.domain.RawContents
 import com.few.generator.service.GenService
 import com.few.generator.service.ProvisioningService
 import com.few.generator.service.RawContentsService
@@ -10,6 +11,7 @@ import com.few.generator.support.jpa.GeneratorTransactional
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import kotlin.system.measureTimeMillis
 
 @Component
 class SchedulingUseCase(
@@ -23,18 +25,46 @@ class SchedulingUseCase(
     @GeneratorTransactional
     fun execute() {
         // 1. 스크래핑 후 Raw 데이터 저장
-        val rawContents = rawContentsService.create()
+        val (rawContents, timeOfCreatingRawContents) =
+            measureAndReturn {
+                rawContentsService.create()
+            }
 
         // 2. raw 데이터 기반 provisioning 생성
-        val provisionings: Map<Category, List<ProvisioningContents>> =
-            rawContents.mapValues { (_, rawContents) ->
-                rawContents.map { rawContent ->
-                    provisioningService.create(rawContent)
-                }
+        val (provisionings, timeOfCreatingProvisionings) =
+            measureAndReturn {
+                createProvisionings(rawContents)
             }
 
         // 3. gen 생성
+        val timeOfCreatingGens =
+            measureTimeMillis {
+                createGens(rawContents, provisionings)
+            }
+
+        log.info {
+            "✅ [1단계] RawContents: ${timeOfCreatingRawContents.msToSeconds()} ms \n" +
+                "✅ [2단계] Provisionings: ${timeOfCreatingProvisionings.msToSeconds()} ms \n" +
+                "✅ [3단계] Gens: ${timeOfCreatingGens.msToSeconds()} ms \n" +
+                "-> 전체 : ${(timeOfCreatingRawContents + timeOfCreatingProvisionings + timeOfCreatingGens).msToSeconds()} ms"
+        }
+
+        // TODO: 스케줄링 결과 이력 저장
+    }
+
+    private fun createProvisionings(rawContents: Map<Category, List<RawContents>>): Map<Category, List<ProvisioningContents>> =
+        rawContents.mapValues { (_, rawContents) ->
+            rawContents.map { rawContent ->
+                provisioningService.create(rawContent)
+            }
+        }
+
+    private fun createGens(
+        rawContents: Map<Category, List<RawContents>>,
+        provisionings: Map<Category, List<ProvisioningContents>>,
+    ) {
         val genTypes: Set<Int> = GenType.entries.map { it.code }.toSet()
+
         rawContents.forEach { (category, rawList) ->
             val provisioningList = provisionings[category].orEmpty()
 
@@ -43,4 +73,13 @@ class SchedulingUseCase(
             }
         }
     }
+
+    private inline fun <T> measureAndReturn(block: () -> T): Pair<T, Long> {
+        val start = System.currentTimeMillis()
+        val result = block()
+        val elapsed = System.currentTimeMillis() - start
+        return result to elapsed
+    }
+
+    private fun Long.msToSeconds(): String = String.format("%.3f", this / 1000.0)
 }
