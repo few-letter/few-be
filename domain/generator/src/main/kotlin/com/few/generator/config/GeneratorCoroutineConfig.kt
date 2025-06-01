7f0a3ff5e74f8d3a062c64f54c805f4d9727db25
Jihun-Hwang <hun.ca.dev@gmail.com>
2025-06-01 22:29:03 +0900

    refactor: 디스코드 코루틴 익스큐터 변경

--- domain/generator/src/main/kotlin/com/few/generator/config/GeneratorCoroutineConfig.kt
+++ domain/generator/src/main/kotlin/com/few/generator/config/GeneratorCoroutineConfig.kt
@@ -1,36 +1,12 @@
 package com.few.generator.config
 
-import jakarta.annotation.PreDestroy
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.Dispatchers
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
 @Configuration
 class GeneratorCoroutineConfig {
-    private lateinit var coroutineScope: CoroutineScope
-
     @Bean
-    fun discordIoCoroutineScope(): CoroutineScope {
-        val executor =
-            ThreadPoolTaskExecutor().apply {
-                corePoolSize = 3
-                maxPoolSize = 3
-                queueCapacity = 10
-                setThreadNamePrefix("discord-io-coroutine-")
-                initialize()
-            }
-        coroutineScope = CoroutineScope(SupervisorJob() + executor.asCoroutineDispatcher())
-        return coroutineScope
-    }
-
-    @PreDestroy
-    fun cleanup() {
-        if (::coroutineScope.isInitialized) {
-            coroutineScope.cancel()
-        }
-    }
+    fun discordIoCoroutineScope(): CoroutineScope = CoroutineScope(Dispatchers.IO.limitedParallelism(2))
 }
