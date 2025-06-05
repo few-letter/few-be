598b2b7e07af54b9b8b675b80b7d188d62f7edfa
Jihun-Hwang <hun.ca.dev@gmail.com>
2025-06-05 23:44:53 +0900

    feat: Feign Client(GPT호출용) Retryer 추가(1초 후 1회 재시도)

--- domain/generator/src/main/kotlin/com/few/generator/config/GeneratorFeignConfig.kt
+++ domain/generator/src/main/kotlin/com/few/generator/config/GeneratorFeignConfig.kt
@@ -1,7 +1,10 @@
 package com.few.generator.config
 
+import feign.Retryer
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableFeignClients(
@@ -9,4 +12,12 @@ import org.springframework.context.annotation.Configuration
         GeneratorConfig.BASE_PACKAGE,
     ],
 )
-class GeneratorFeignConfig
+class GeneratorFeignConfig {
+    @Bean
+    fun feignRetryer(): Retryer =
+        Retryer.Default(
+            TimeUnit.SECONDS.toMillis(1),
+            TimeUnit.SECONDS.toMillis(1),
+            1,
+        )
+}
