73d3cf936cc38b35c0278de7cfa32176ffde973c
Jihun <hun.ca.dev@gmail.com>
2025-06-24 21:36:46 +0900

    [Feat/#669] 컨텐츠 조회 무한스크롤 조회 구현 (#674)
    * feat: 컨텐츠 페이징 조회 컨트롤러 구현
    
    * feat: gen 엔티티에 인덱스 추가 (created_at 컬럼 desc)
    
    * feat: 컨텐츠 무한스크롤 조회 1차 구현
    
    * feat: 컨텐츠 조회 API 시큐리티 적용
    
    * feat: 첫 번쨰 페이지에 대해 예외처리 로직 추가
    
    * fix: WRITE_DATES_AS_TIMESTAMPS 오브젝트 매퍼 설정 끄도록 변경
    
    * fix: 첫번쨰 페이지 로드 쿼리 수정 (id -> created_dt 기준 정렬)
    
    * refactor: 메소드 명 수정
    
    * refactor: 페이징 쿼리 성능 개선
    
    * fix: 쿼리 파라메터 명 수정

---
+++ domain/generator/src/main/kotlin/com/few/generator/controller/response/BrowseContentResponses.kt
@@ -0,0 +1,22 @@
+package com.few.generator.controller.response
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import java.time.LocalDateTime
+
+data class BrowseContentResponses(
+    val contents: List<BrowseContentResponse>,
+    val isLast: Boolean,
+)
+
+data class BrowseContentResponse(
+    val id: Long,
+    val url: String,
+    val thumbnailImageUrl: String?,
+    val mediaType: CodeValueResponse,
+    val headline: String,
+    val summary: String,
+    val highlightTexts: List<String>,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    val createdAt: LocalDateTime,
+    val category: CodeValueResponse,
+)
--- domain/generator/src/main/kotlin/com/few/generator/repository/GenRepository.kt
+++ domain/generator/src/main/kotlin/com/few/generator/repository/GenRepository.kt
@@ -2,9 +2,35 @@ package com.few.generator.repository
 
 import com.few.generator.domain.Gen
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface GenRepository : JpaRepository<Gen, Long> {
     fun findByProvisioningContentsId(provisioningContentsId: Long): List<Gen>
 
     fun existsByProvisioningContentsId(provisioningContentsId: Long): Boolean
+
+    @Query(
+        """
+        SELECT g.* FROM gen g
+        WHERE g.created_at < (
+            SELECT created_at FROM gen WHERE id = :targetId
+        )
+        ORDER BY g.created_at DESC
+        LIMIT :limitSize
+        """,
+        nativeQuery = true,
+    )
+    fun findNextLimit(
+        @Param("targetId") targetId: Long,
+        @Param("limitSize") limitSize: Int,
+    ): List<Gen>
+
+    @Query(
+        "SELECT * FROM gen ORDER BY created_at DESC LIMIT :limitSize",
+        nativeQuery = true,
+    )
+    fun findFirstLimit(
+        @Param("limitSize") limitSize: Int,
+    ): List<Gen>
 }
