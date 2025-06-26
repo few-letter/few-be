4cc3f779844459b0d5717d0697f7da7cda049665
belljun3395 <195850@jnu.ac.kr>
2025-06-26 23:34:17 +0900

    [Refactor/#678] Scrapper의 Naver 강결합 로직 분리 (#680)
    * refactor: rootUrl 삭제
    
    * feat: ImageSuffix 추가
    
    * feat: Naver 스크래핑 로직 분리
    
    * feat: RetryAbleJsoup 분리
    
    * refactor: Scrapper 패키지 이동 및 분리 반영
    
    * fix: 기존 정규식 패턴 적용
    
    * refactor: 리뷰 반영
    
    * refactor: 리뷰 반영
    
    * refactor: 스크래핑 시작전 슬립 코드 분리

---
+++ domain/generator/src/main/kotlin/com/few/generator/core/scrapper/naver/NaverScrapper.kt
@@ -0,0 +1,57 @@
+package com.few.generator.core.scrapper.naver
+
+import com.few.generator.core.connection.RetryableJsoup
+import com.few.generator.core.scrapper.ScrappedResult
+import com.few.generator.domain.Category
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.jsoup.nodes.Document
+import org.springframework.stereotype.Service
+
+@Service
+class NaverScrapper(
+    private val retryableJsoup: RetryableJsoup,
+) {
+    private val log = KotlinLogging.logger { }
+
+    fun getRootUrlsByCategory(categories: List<Category>): Map<Category, String> =
+        categories
+            .filter { it in NaverConstants.ROOT_URL_MAP }
+            .associateWith { NaverConstants.ROOT_URL_MAP[it]!! }
+
+    fun extractUrlsByCategory(rootUrl: String): Set<String> {
+        // Introduce a random sleep time to avoid hitting the server too quickly
+        Thread.sleep((1..5).random().toLong())
+        return retryableJsoup
+            .connect(rootUrl)
+            .select("a[href]")
+            .mapNotNull { it.attr("href") }
+            .filter { it.matches(NaverConstants.NEWS_URL_REGEX) }
+            .map { it.replace("amp;", "") }
+            .toSet()
+    }
+
+    fun parseDocument(document: Document): ScrappedResult? {
+        removeUnnecessaryTags(document)
+        val mainContent = document.selectFirst("main, article, div.content") ?: document
+        val title = document.selectFirst("title")?.text()?.trim()
+        val description = document.selectFirst("meta[name=description]")?.attr("content")?.trim()
+        val thumbnailImageUrl =
+            document
+                .selectFirst("meta[property=og:image]")
+                ?.attr("content")
+                ?.trim()
+                .orEmpty()
+        val rawTexts = NaverExtractor.Text.extract(mainContent)
+        val images = NaverExtractor.Image.extract(document)
+
+        if (title.isNullOrBlank() || description.isNullOrBlank()) {
+            log.error { "title 및 description 스크래핑 실패. URL: ${document.location()}" }
+            return null
+        }
+        return ScrappedResult(title, description, thumbnailImageUrl, rawTexts, images)
+    }
+
+    private fun removeUnnecessaryTags(document: Document) {
+        document.select("script, style, nav, footer, header").forEach { it.remove() }
+    }
+}
