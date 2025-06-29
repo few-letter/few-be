2b3dba1ce25042fea5d1d60824cf6ee0349aaf67
Jihun-Hwang <hun.ca.dev@gmail.com>
2025-06-29 12:07:05 +0900

    feat: gen highlightTexts 추가

--- domain/generator/src/main/kotlin/com/few/generator/service/GenService.kt
+++ domain/generator/src/main/kotlin/com/few/generator/service/GenService.kt
@@ -1,15 +1,19 @@
 package com.few.generator.service
 
+import com.few.generator.config.GeneratorGsonConfig.Companion.GSON_BEAN_NAME
 import com.few.generator.core.gpt.ChatGpt
 import com.few.generator.core.gpt.prompt.PromptGenerator
 import com.few.generator.core.gpt.prompt.schema.Headline
+import com.few.generator.core.gpt.prompt.schema.HighlightText
 import com.few.generator.core.gpt.prompt.schema.Summary
 import com.few.generator.domain.Category
 import com.few.generator.domain.Gen
 import com.few.generator.domain.ProvisioningContents
 import com.few.generator.domain.RawContents
 import com.few.generator.repository.GenRepository
+import com.google.gson.Gson
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
 @Service
@@ -17,6 +21,8 @@ class GenService(
     private val promptGenerator: PromptGenerator,
     private val chatGpt: ChatGpt,
     private val genRepository: GenRepository,
+    @Qualifier(GSON_BEAN_NAME)
+    private val gson: Gson,
 ) {
     private val log = KotlinLogging.logger {}
 
@@ -41,12 +47,16 @@ class GenService(
             )
         val summary: Summary = chatGpt.ask(summaryPrompt) as Summary
 
+        val highlightTextPrompt = promptGenerator.toKoreanHighlightText(summary.summary)
+        val highlightText: HighlightText = chatGpt.ask(highlightTextPrompt) as HighlightText
+
         return genRepository.save(
             Gen(
                 provisioningContentsId = provisioningContent.id!!,
-                completionIds = mutableListOf(headline.completionId!!, summary.completionId!!),
+                completionIds = mutableListOf(headline.completionId!!, summary.completionId!!, highlightText.completionId!!),
                 headline = headline.headline,
                 summary = summary.summary,
+                highlightTexts = "[${highlightText.highlightText}]",
                 category = Category.from(provisioningContent.category).code,
             ),
         )
