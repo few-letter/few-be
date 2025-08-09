e1bf7942c804d8578f8074342ed3d9bbc390b8cc
Jihun-Hwang <hun.ca.dev@gmail.com>
2025-08-09 19:42:01 +0900

    refactor: GeneratorTransactional 클래스 일부 수정 (중복코드)

--- domain/provider/src/main/kotlin/com/few/provider/support/jpa/GeneratorTransactional.kt
+++ domain/provider/src/main/kotlin/com/few/provider/support/jpa/GeneratorTransactional.kt
@@ -5,7 +5,6 @@ import org.springframework.core.annotation.AliasFor
 import org.springframework.transaction.annotation.Isolation
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
-import kotlin.reflect.KClass
 
 /**
  * Api DataSource 트랜잭션을 처리하는 어노테이션
@@ -15,24 +14,10 @@ import kotlin.reflect.KClass
 @Retention(AnnotationRetention.RUNTIME)
 @Transactional(transactionManager = ProviderJpaConfig.TRANSACTION_MANAGER)
 annotation class ProviderTransactional(
-    @get:AliasFor(annotation = Transactional::class, attribute = "label")
-    val label: Array<String> = [],
     @get:AliasFor(annotation = Transactional::class, attribute = "propagation")
     val propagation: Propagation = Propagation.REQUIRED,
     @get:AliasFor(annotation = Transactional::class, attribute = "isolation")
     val isolation: Isolation = Isolation.DEFAULT,
-    @get:AliasFor(annotation = Transactional::class, attribute = "timeout")
-    val timeout: Int = -1,
-    @get:AliasFor(annotation = Transactional::class, attribute = "timeoutString")
-    val timeoutString: String = "",
     @get:AliasFor(annotation = Transactional::class, attribute = "readOnly")
     val readOnly: Boolean = false,
-    @get:AliasFor(annotation = Transactional::class, attribute = "rollbackFor")
-    val rollbackFor: Array<KClass<out Throwable>> = [],
-    @get:AliasFor(annotation = Transactional::class, attribute = "rollbackForClassName")
-    val rollbackForClassName: Array<String> = [],
-    @get:AliasFor(annotation = Transactional::class, attribute = "noRollbackFor")
-    val noRollbackFor: Array<KClass<out Throwable>> = [],
-    @get:AliasFor(annotation = Transactional::class, attribute = "noRollbackForClassName")
-    val noRollbackForClassName: Array<String> = [],
 )
