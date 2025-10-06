76a794d0132c1134e8831f432789f2f9867e638b
eden6187 <56511173+eden6187@users.noreply.github.com>
2025-10-06 21:27:28 +0900

    [Fix/#768] email 전송 기능 하드닝 (#770)
    * fix: newsletter.html 내용 수정
    
    * fix: 기사 원문 사이트 표시 되도록 수정
    
    * fix: 카테고리 제목 좌측에 이모지 노출될 수 있도록 로직 변경
    
    * fix: 이메일 제목을 1번째로 구독한 카테고리의 첫번째 뉴스 헤드라인으로 변경
    
    * refactor: 패키지 구조 변경
    
    * fix: 템플릿 오타 수정
    
    * refactor: cron 식 property로 추출

--- domain/generator/src/main/kotlin/com/few/generator/service/specifics/newsletter/NewsletterTemplateData.kt
+++ domain/generator/src/main/kotlin/com/few/generator/service/specifics/newsletter/NewsletterTemplateData.kt
@@ -1,4 +1,4 @@
-package com.few.generator.service.implement
+package com.few.generator.service.specifics.newsletter
 
 import com.few.common.domain.Category
 import com.few.email.GenData
@@ -20,6 +20,8 @@ data class NewsletterTemplateData(
                     CategoryGenData(
                         categoryName = Category.from(categoryCode).title,
                         gens = gens,
+                        categoryCode = categoryCode,
+                        categoryEmoji = emoji(Category.from(categoryCode)),
                     )
                 }
 
@@ -28,10 +30,22 @@ data class NewsletterTemplateData(
                 gensByCategory = categoryGenDataList,
             )
         }
+
+        fun emoji(category: Category): String =
+            when (category) {
+                Category.TECHNOLOGY -> "\uD83D\uDCBB" // 💻
+                Category.LIFE -> "\uD83C\uDFE1" // 🏡
+                Category.POLITICS -> "\uD83C\uDFDB\uFE0F" // 🏛️
+                Category.ECONOMY -> "\uD83D\uDCB0" // 💰
+                Category.SOCIETY -> "\uD83C\uDF0E" // 🌎
+                else -> "\uD83D\uDD2E" // 🔮
+            }
     }
 }
 
 data class CategoryGenData(
+    val categoryCode: Int,
     val categoryName: String,
     val gens: List<GenData>,
+    val categoryEmoji: String,
 )
